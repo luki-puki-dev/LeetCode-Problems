04f74955f2402ff6aa00e010e5a64ab55dc3394e
Bujenita Lucian-Andrei <lucian312003@gmail.com>
2026-08-20 23:42:51 +0300

    Distribute Elements Into Two Arrays I

---
+++ Distribute Elements Into Two Arrays I.py
@@ -0,0 +1,21 @@
+nums = [1,2,14,15]
+arr1 = []
+arr2 = []
+n = len(nums)
+i = 1
+for item in nums:
+    if i == 1:
+        arr1.append(item)
+    elif i == 2:
+        arr2.append(item)
+    else:
+        if arr1[-1] > arr2[-1]:
+            arr1.append(item)
+        else:
+            arr2.append(item)
+    
+    i += 1
+
+    
+result = arr1 + arr2
+print(result)
