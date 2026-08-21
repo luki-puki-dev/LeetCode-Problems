cd5218898b74c7a5f2b984d2cbf336e25e45be61
Bujenita Lucian-Andrei <lucian312003@gmail.com>
2026-08-22 00:23:58 +0300

    Kth Smallest Amount With Single Denomination Combination

--- Kth Smallest Amount With Single Denomination Combination.py
+++ Kth Smallest Amount With Single Denomination Combination.py
@@ -1,17 +1,23 @@
+
 coins = [4,8]
 k = 55789
 
-result = 0
-l = []
+coins = sorted(coins)
+l = set()
 i = 0
+req = coins[-1] ** coins[0]
 while(True):
     i += 1
     for coin in coins:
-       if (coin * i) not in l:
-           l.append(coin*i)
+        l.add(coin*i)
     
-    if len(l) >= k:
+    if len(l) >= k and i > req:
         break
 
-l = sorted(l)
-print(l[k-1])
+l = sorted(list(l))
+print(l,l[k-1])
+
+5, 10, 15, 20, 25, 30, 35
+2, 4, 6, 8, 10, 12, 14
+
+2, 4, 5, 6, 8, 10, 12, 14, 15
