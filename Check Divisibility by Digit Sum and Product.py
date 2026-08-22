b414e5a6831f9d9dc7dc65acf0f155f4d14c195b
Bujenita Lucian-Andrei <lucian312003@gmail.com>
2026-08-22 19:48:07 +0300

    Check Divisibility by Digit Sum and Product v2

--- Check Divisibility by Digit Sum and Product.py
+++ Check Divisibility by Digit Sum and Product.py
@@ -1,15 +1,18 @@
-n = 99
-
-result = False
-n2 = []
-n2 = [int(x) for x in str(n)]
-
-Sum = sum(n2)
-Prod = 1
-
-for digit in n2:
-    Prod *= digit
-print(Sum,Prod)
-if n % (Sum + Prod) == 0:
-    result = True
-print(result)
+class Solution(object):
+    def checkDivisibility(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        result = False
+        n2 = []
+        n2 = [int(x) for x in str(n)]
+        Sum = sum(n2)
+        Prod = 1
+        for digit in n2:
+            Prod *= digit
+        if n % (Sum + Prod) == 0:
+            result = True
+        return result
+        
+        
