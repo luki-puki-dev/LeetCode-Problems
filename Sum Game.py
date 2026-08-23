bee8538d0365663540e30604b891cd0ad7d9d757
Bujenita Lucian-Andrei <lucian312003@gmail.com>
2026-08-23 22:56:54 +0300

    Sum Game last version

--- Sum Game.py
+++ Sum Game.py
@@ -1,53 +1,28 @@
-num = "9?"
+class Solution(object):
+    def sumGame(self, num):
 
-if "?" not in num:
-    num = list(map(int,str(num)))
-    halfSize = len(num) // 2
-    if sum(num[:halfSize]) == sum(num[-halfSize:]):
-        print("false")
-        #return False
-    else:
-        print("true")
-        #return True
-        
-elif num.count("?") % 2 != 0:
-        print("true")
-        #return True
-else:
-    num = list(map(str,str(num)))
-    halfSize = len(num) // 2
-    questionMarksFirstHalf = num[:halfSize].count("?")
-    questionMarksSecondHalf = num[halfSize:].count("?")
-    sumFirstHalf = 0
-    for item in num[:halfSize]:
-        if item != "?":
-            sumFirstHalf += int(item)
+        if num.count("?") % 2 != 0:
+            return True
             
-    sumSecondHalf = 0
-    for item in num[halfSize:]:
-        if item != "?":
-            sumSecondHalf += int(item)
-    if questionMarksFirstHalf > questionMarksSecondHalf:
-        print("true - alice wins")
-        #return True
-    elif questionMarksFirstHalf == 0:
+
+        num = list(str(num))
+        halfSize = len(num) // 2
         
-        if( sumFirstHalf - sumSecondHalf) == (9/2)*questionMarksSecondHalf:
-            print("false - bob wins1")
-            #return False
-        elif (sumFirstHalf - sumSecondHalf) > (9/2)*questionMarksSecondHalf:
-            print("true - alice wins")
-            #return True
-        elif (sumFirstHalf - sumSecondHalf) < (9/2)*questionMarksSecondHalf:
-            print("true - alice wins")
-            #return True
-    elif questionMarksFirstHalf != 0:
-        if (sumFirstHalf-sumSecondHalf)  == (9/2)*(questionMarksSecondHalf-questionMarksFirstHalf):
-            print("false - bob wins2")
-            #return False
-        elif (sumFirstHalf-sumSecondHalf)  > (9/2)*(questionMarksSecondHalf-questionMarksFirstHalf):
-            print("true - alice wins")
-            #return True
-        elif (sumFirstHalf-sumSecondHalf)  < (9/2)*(questionMarksSecondHalf-questionMarksFirstHalf):
-            print("true - alice wins")
-            #return true
+
+        questionMarksFirstHalf = num[:halfSize].count("?")
+        questionMarksSecondHalf = num[halfSize:].count("?")
+        
+
+        sumFirstHalf = 0
+        for item in num[:halfSize]:
+            if item != "?":
+                sumFirstHalf += int(item)
+                
+        sumSecondHalf = 0
+        for item in num[halfSize:]:
+            if item != "?":
+                sumSecondHalf += int(item)
+        if (sumFirstHalf - sumSecondHalf) == 4.5 * (questionMarksSecondHalf - questionMarksFirstHalf):
+            return False
+        else:
+            return True
