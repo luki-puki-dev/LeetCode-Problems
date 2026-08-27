1a11c10e3498f8efe3d3490743f6889754a8fe53
Bujenita Lucian-Andrei <lucian312003@gmail.com>
2026-08-28 00:22:47 +0300

    Lexicographically Smallest Permutation Greater Than Target v2

--- Lexicographically Smallest Permutation Greater Than Target.py
+++ Lexicographically Smallest Permutation Greater Than Target.py
@@ -1,14 +1,60 @@
 from itertools import permutations
-s = "leet"
-target = "code"
-result = ""
-permutation = permutations(s)
-for i in sorted(permutation):
-    x = "".join(i)
-    if x > target:
-        result = x
-        break
-    
-print(result)
 
-# s[i]  s[i+1:]    s[:0+i]
+class Solution(object):
+    def lexGreaterPermutation(self, s, target):
+        """
+        :type s: str
+        :type target: str
+        :rtype: str
+        """
+        counts = {}
+        for char in s:
+            counts[char] = counts.get(char, 0) + 1
+            
+        n = len(s)
+        res = []
+        
+        
+        for i in range(n):
+            t_char = target[i] if i < len(target) else ''
+            
+
+            available_chars = sorted([k for k, v in counts.items() if v > 0])
+            placed = False
+            
+            for c in available_chars:
+                if c > t_char:
+
+                    res.append(c)
+                    counts[c] -= 1
+                    
+
+                    for rem_c in sorted(counts.keys()):
+                        if counts[rem_c] > 0:
+                            res.append(rem_c * counts[rem_c])
+                    return "".join(res)
+                
+                elif c == t_char:
+
+                    counts[c] -= 1
+                    
+                    max_remaining_list = []
+                    for k in sorted(counts.keys(), reverse=True):
+                        if counts[k] > 0:
+                            max_remaining_list.append(k * counts[k])
+                    max_remaining = "".join(max_remaining_list)
+                    
+                    target_remaining = target[i+1:]
+                    
+                    if max_remaining > target_remaining:
+                        res.append(c)
+                        placed = True
+                        break
+                    else:
+
+                        counts[c] += 1
+            
+            if not placed:
+                return ""
+                
+        return "".join(res)
